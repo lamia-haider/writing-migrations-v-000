48dbd0d90ffbe2120cd394eca27978f2f88ef58a
Lamia Haider <lamia.haider@gmail.com>
2020-08-19 03:30:05 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -4,8 +4,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
-      t.integer :grade
-      t.integer :birthdate
     end
   end
 
